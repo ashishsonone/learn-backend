25545b4338d67e65f10addda240cd66c67f6cd90
Ashish Sonone <ashson07@gmail.com>
2016-02-03 13:00:23 +0530

    mongodb_reconnect : understand mongoose debug, mixed type save using markModified()

--- mongodb_reconnect/index.js
+++ mongodb_reconnect/index.js
@@ -5,14 +5,18 @@ var mongoose = require('mongoose');
 var app = express();
 
 var userSchema = mongoose.Schema({
-    name : String
+    name : String,
+    age : Number,
+    liked : {} //mixed can be boolean true/false or can be a neutral comment string, or can be rating(number) from 1 to 10
 });
+
+var i = Math.floor(Math.random() * 100);
 
 var userModel = mongoose.model('user', userSchema);
 
 function connectWithRetry(){
     attempting = true;
-    mongoose.connect("mongodb://localhost:27017/delete", {server:{auto_reconnect:false, socketOptions: {connectTimeoutMS: 5000, socketTimeouMS : 5000}}}, function(err){
+    mongoose.connect("mongodb://localhost:27018/delete", {server:{auto_reconnect:false, socketOptions: {connectTimeoutMS: 5000, socketTimeouMS : 5000}}}, function(err){
         if (err) {
             console.log("(retry in 5s)connectWithRetry error %j", err);
             setTimeout(connectWithRetry, 5000);
@@ -72,6 +76,38 @@ app.get('/', function(req, res){
         else{
             res.json({"error" : "db connection error", "debug" : err});
         }
+    });
+});
+
+app.get('/:name', function(req, res){
+    var newUser = new userModel();
+    newUser.name = req.params.name;
+    newUser.age = i++;
+    newUser.save(function(err, user){
+        console.log("new user : %j", user);
+        res.json(user);
+    });
+});
+
+app.get('/update/:name', function(req, res){
+    userModel.findOne({name : req.params.name}, function(err, newUser){
+        newUser.age = i++;
+        var likedType = Math.floor(Math.random()*3);
+        if(likedType == 0){//boolean
+            newUser.liked = true;
+        }
+        else if(likedType == 1){//number
+            newUser.liked = i++;
+        }
+        else{//string
+            newUser.liked = "my view is it will earn rs " + (i++) + " cr";
+        }
+        newUser.markModified('liked');   //since like is mixed type, its not being tracked 
+                                        //by mongoose, we need to explicitly tell it
+        newUser.save(function(err, user){
+            console.log("new user : %j", user);
+            res.json(user);
+        });
     });
 });
 
