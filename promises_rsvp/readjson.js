d11601a8f9c6e4edf4e180f12693d66a0dbde3a5
Ashish Sonone <ashson07@gmail.com>
2016-02-10 12:17:30 +0530

    promises_rsvp : attempt deep understand by printing promise objects

--- promises_rsvp/readjson.js
+++ promises_rsvp/readjson.js
@@ -2,30 +2,49 @@
 var fs = require('fs');
 var RSVP = require('rsvp');
 
+function wait(time, data){
+    var prom = new RSVP.Promise(function(fulfill, reject){
+        setTimeout(function(){
+            console.log("over wait promise " + time + " " + data);
+            fulfill(data);
+        },
+        time);
+    });
+    console.log("enter wait promise " + time + " " + data + " %j", prom);
+    return prom;
+}
+
 function parse(data){
-    return RSVP.Promise.resolve().then(
+    var prom = RSVP.Promise.resolve().then(
         function(){
+            console.log("1");
             return JSON.parse(data);
         }
-    ).then(function wait(data){
-        console.log("wait data=" + data);
-        return new RSVP.Promise(function(f, r){
-            setTimeout(function(){
-                f(data);
-            }, 3000);
-        });
+    ).then(function (data){
+        console.log("2");
+        return wait(3000, data);
     });
+    
+    console.log("over here %j", prom);
+    prom = prom.then(function(data){
+        console.log("3");
+        return wait(2000, data);
+    });
+    console.log("returing parse prom %j", prom);
+    return prom;
 }
 
 function readFile(f){
-    return new RSVP.Promise(function(fulfill, reject){
+    var prom = new RSVP.Promise(function(fulfill, reject){
         fs.readFile(f, 'utf8', function (err, data) {
             if(err){
                 reject(err);
                 console.log("reject 1");
                 return;
             }
-            parse(data).then(function(obj){
+            var p = parse(data);
+            console.log("called parse %j", p);
+            p.then(function(obj){
                 if(obj) {
                     fulfill(obj);
                     console.log("fulfill 1");
@@ -39,8 +58,12 @@ function readFile(f){
             }, function(err){
                 reject(err);
             });
+            console.log("called parse %j", p);
         });
     });
+
+    console.log("returing readFile prom %j", prom);
+    return prom;
 }
 
 function success(data){
@@ -51,6 +74,6 @@ function failure(err){
     console.log("failure : " + err);
 }
 
-var p = readFile('a.json');
-p.then(success, failure);
-console.log('hello');
+var prom = readFile('a.json');
+var then = prom.then(success, failure);
+console.log("EOF %j then=%j", prom, then);
